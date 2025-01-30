da2a82b4636f3f9d8060f083f3eeeeae3b2ba8d8
bagusdyuda <bagusdwiwahyuda@gmail.com>
2025-01-30 14:20:24 +0700

    add role dan isi

--- database/migrations/2023_03_30_040040_create_barangs_table.php
+++ database/migrations/2023_03_30_040040_create_barangs_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('barangs', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
+            $table->integer('isi');
             $table->timestamps();
         });
     }
